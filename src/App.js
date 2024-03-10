ccbdfad32baf1ced7e1f0bb85211f45b54786b6f
Anand Soni <aanandsoni50@gmail.com>
2024-03-10 20:44:01 +0530

    Adding all components to root

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import InputForm from "./components/InputForm";
+import QRcode from "./components/QRcode";
 
 function App() {
   return (
-    <div className="text-red-700">
-      <h1>LinkQRator</h1>
+    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-screen flex items-center">
+      <div className="container mx-auto max-w-4xl bg-cyan-300 rounded-md shadow">
+        <div className="md:grid md:grid-cols-3">
+          <InputForm />
+          <QRcode />
+        </div>
+      </div>
     </div>
   );
 }
