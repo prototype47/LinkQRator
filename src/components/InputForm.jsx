e95a3cfd53a6451ea90ec0ac281403b634c1487f
Anand Soni <aanandsoni50@gmail.com>
2024-03-11 16:58:54 +0530

    logic for button

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -5,7 +5,7 @@ import { InputContext } from '../App';
 
 const InputForm = () => {
 
-    const { getQRcode } = useContext(InputContext);
+    const { getQRcode, inputValue } = useContext(InputContext);
 
     const handleSubmit = () => getQRcode();
 
@@ -13,7 +13,7 @@ const InputForm = () => {
         <div className='col-span-2 p-6 grid gap-4'>
             <InputField />
             <InputColor />
-            <button onClick={handleSubmit} className='bg-blue-400 max-w-xs ml-auto px-4 py-2 text-white rounded-md mt-4 hover:bg-blue-500 disabled:bg-gray-500 '>Generate QR</button>
+            <button disabled={!inputValue.url} onClick={handleSubmit} className='bg-blue-400 max-w-xs ml-auto px-4 py-2 text-white rounded-md mt-4 hover:bg-blue-500 disabled:bg-gray-500 '>Generate QR</button>
         </div>
     )
 }
